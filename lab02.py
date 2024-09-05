7a5963b5c0a92d0f91d41eaa973b71f2df8eb45c
Nick Wren <nckwrn1@gmail.com>
2024-09-05 11:23:21 -0700

    Part 2

--- lab02.py
+++ lab02.py
@@ -1,4 +1,20 @@
-name = Nick
+#Part 1
+name = "Nick"
 age = 29
 height = 5.66666666666
-favorite_color = Purple
+favorite_color = "Purple"
+print (name)
+print (age)
+print (height)
+print (favorite_color)
+print (name,age,height,favorite_color)
+print (f"{name}") #not done
+print (f"""
+Name: {name}
+Age: {age}
+Height: {height}
+Favorite Color: {favorite_color}""")
+import math
+circle_area = math.pi * 5**2
+print (f"{circle_area:.1f}")
+#Part 2
