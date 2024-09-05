2a5cf5a932a1fae286a19660b0112111c45c70ad
Nick Wren <nckwrn1@gmail.com>
2024-09-05 12:46:51 -0700

    ALmost done

---
+++ Temperature Conversion.py
@@ -0,0 +1,4 @@
+f = int(input("Enter a Temperature in Fahrenheit to Convert it to Celsius : ").strip())
+print (f)
+c = ((f-32)*(5/9))
+print (f"{c:.1f}")
--- lab02.py
+++ lab02.py
@@ -1,4 +1,5 @@
 #Part 1
+import math
 name = "Nick"
 age = 29
 height = 5.66666666666
@@ -8,13 +9,12 @@ print (age)
 print (height)
 print (favorite_color)
 print (name,age,height,favorite_color)
-print (f"{name}") #not done
+print (f"{name}") #not done make a normal sentence stating like my name is, i am
 print (f"""
 Name: {name}
 Age: {age}
 Height: {height}
 Favorite Color: {favorite_color}""")
-import math
 circle_area = math.pi * 5**2
 print ("Area of a Circle with Radius 5 Rounded to One Decimal Place:", f"{circle_area:.1f}")
 #Part 2
@@ -26,8 +26,8 @@ print ("Sum of Age and 5 :",(age+5))
 print ("Difference Between Height and 4 :", height-4)
 print ("Product of Age and Height : ", age*height )
 print ("Quotient of Height and 2 : ", height/2)
-print ("Remainder of age Divided by 3 : ", age/3) #not right
+print ("Remainder of age Divided by 3 : ", age%3)
 print ("Age Raised to the Power of 2 : ", age**2)
-#Part 4
+#Part 4 *Seperate File*
 
 
